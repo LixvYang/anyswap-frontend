84683a8084beb4884fdeba8a5d9f3bf9b21c5160
LixvYang <2690688423@qq.com>
2024-07-13 10:46:03 +0800

    update anyswap

--- src/lib/actions.ts
+++ src/lib/actions.ts
@@ -1,16 +1,22 @@
 "use server";
 
 export async function getAnySwapCoinList() {
-  const coinList = await fetch("/api/tokens", {
-    cache: "default",
-  });
+  const coinList = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/tokens`,
+    {
+      cache: "default",
+    }
+  );
   return coinList.json();
 }
 
 export const fetchMixswapTokenList = async () => {
-  const response = await fetch("/api/mixin/tokens", {
-    next: { revalidate: 3600 },
-  });
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/mixin/tokens`,
+    {
+      next: { revalidate: 3600 },
+    }
+  );
   const tokenList = await response.json();
   return tokenList.data;
 };
